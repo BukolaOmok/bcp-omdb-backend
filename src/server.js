027cf85c1766c729866fb5ac1d39ce0014be18b4
2405-005-bo <omokehindeoluwabukola@gmail.com>
2024-07-26 15:03:32 +0100

    KAN-23 code to post a comment post a comment- WIP

--- src/server.js
+++ src/server.js
@@ -25,6 +25,21 @@ app.get("/movies/search", async (req, res) => {
     }
 });
 
+app.post("/movies/:movie_id/comments", async (req, res) => {
+    try {
+        const movieID = parseInt(req.params.movie_id);
+        const commentBody = req.body;
+
+        const dbResult = await query(
+            "INSERT INTO comments (movie_id, author, comment_text) VALUES ($1, $2, $3) RETURNING *",
+            [movieID, commentBody.author, commentBody.comment_text]
+        );
+        res.json(dbResult.rows);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+});
+
 // use the environment variable PORT, or 4000 as a fallback
 const PORT = process.env.PORT ?? 4000;
 
